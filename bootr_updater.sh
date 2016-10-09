aec52c6d73e17bc936ed2b1511778e94c9a615b6
FroopleXP <connor@frooplexp.com>
2016-10-09 17:24:07 +0100

    Fixed updater

--- bootr_updater.sh
+++ bootr_updater.sh
@@ -1,6 +1,6 @@
 sudo git pull origin master
 sudo rm /bin/rom_bootr.py
-sudo cp /install_files/rom_bootr.py /bin/
+sudo cp install_files/rom_bootr.py /bin/
 sudo rm /boot/ROMS/_config/lib_cores.json
-sudo cp /install_files/lib_cores.json /boot/ROMS/_config/
+sudo cp install_files/lib_cores.json /boot/ROMS/_config/
 sudo reboot now
